3cf7f61063f5108092b40fc9d7adb44de7950704
Krish Shrma <krishhq7@gmail.com>
2025-10-30 02:20:09 +0530

    Add files via upload

---
+++ 01_first_code.py
@@ -0,0 +1,7 @@
+print("One day, this code will change my life")
+
+# how to add a comment
+# comment are for people to read - not for python
+
+''' created by : Krish
+    thanks for reading'''
---
+++ 02_variables_and_datatypes.py
@@ -0,0 +1,57 @@
+''' 
+today i learned about variables and data types in python.
+variables are like boxes where we store information'''
+
+# storing information in variable
+a = "krish"
+b = 12
+c = 23.23
+d = True
+e = False
+f = None
+
+# printing the value
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+
+# as you can see when we print it Python shows whatever is stored in the variable.
+
+# rules for variable name
+'''
+first : only alphabate , digits and underscores
+second : starts with alphabate or underscores
+third : can't start with a digit
+fourth : no spacing is allowed in variable name
+fifth : variable are case sensitive i.e - A and a are different'''
+
+# data type : stuff that are stored in variable
+'''
+Integers 1234                    class<int
+Floating point numbers 23.23     class<float
+String hello                     class<str
+booleans true false              class<bool
+None none                        class<nonetype
+
+'''
+
+# ways to write string
+'''
+a = 'krish'
+a = "krish"
+a = ' "krish" '
+'''
+
+# printing the types of variables
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+
+# keywords are reserved words in python like : true, false, del, def, break, if, else, while, break, and, or, etc.
+# identifiers : Names you give to variables, functions, or classes.
