37a3b7795875487ef3bb6b1c8f77148e6c3e6259
Krish Shrma <krishhq7@gmail.com>
2025-11-02 03:21:01 +0530

    Add files via upload

---
+++ 03_input_and_output.py
@@ -0,0 +1,30 @@
+'''
+today i learned how to take input from the user
+and show it back using print().
+this is how python starts a real conversation with you 😄
+'''
+
+# taking input from the user
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+# showing the data back
+print("Hello", name)
+print("You are", age, "years old.")
+
+# as you can see, when we print it, python shows whatever is stored in the variable.
+
+'''
+notes:
+input() is used to take data from the user.
+by default, input() returns a string.
+print() is used to show messages or values on the screen.
+'''
+
+# example: combining both
+print("Nice to meet you,", name + "!")
+
+'''
+this was fun now python doesn’t just talk,
+it listens too
+'''
