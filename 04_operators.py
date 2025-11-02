2dce791ad17a997237ae36732f39330cd0496eb1
Krish Shrma <krishhq7@gmail.com>
2025-11-03 00:41:16 +0530

    Add files via upload

---
+++ 04_operators.py
@@ -0,0 +1,55 @@
+# Today, I learned about operators.
+
+a = 12
+b = 34.23
+c = 32
+d = 90
+
+# Arithematic operators
+
+print("Arithematic Operators")
+print('The sum and c + b is', c + b)
+print("The difference of b and c is", b - c)
+print("The product of b and c is", b * c)
+print("The division of b and c is", b / c)
+
+# Arithematic operators
+
+print("The value of 3 + 4 is", 3 + 4)
+print('The value of 3 - 4 is', 3 - 4)
+print("The value of 3 * 4 is", 3 * 4)
+print("The value of 3 / 4 is", 3 / 4)
+
+# Assignment Operators
+print("Assignments Operator")
+a += 4
+b *= 3
+c /= 2
+d -= 6
+print(a)
+print(b)
+print(c)
+print(d)
+
+# Comparison Operators
+e = (4>2)
+f = (2<1)
+g = (4>=2)
+h = (4<=2)
+i = (4==2)
+j = (14!=7)
+print (e)
+print (f)
+print (g)
+print (h)
+print (i)
+print ("J", j)
+
+# Logical Operators
+bool1= True
+bool2= False
+print("THE VALUE OF BOOL1 AND BOOL2 IS", (bool1 and bool2))
+print("the value of bool1 and bool2 is", (bool1 or bool2))
+print("the value of bool1 and bool2 is ", not bool2)
+
+
