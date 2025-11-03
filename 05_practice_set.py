9e367e700a0167982836827beadf440ba769cb1e
Krish Shrma <krishhq7@gmail.com>
2025-11-04 00:50:10 +0530

    Add files via upload

---
+++ 05_practice_set.py
@@ -0,0 +1,18 @@
+'''
+so today i just practiced what i learned till now.
+this small example uses print() and multi-line strings together.
+'''
+
+print('''Jingle bells, jingle bells,
+Jingle all the way!
+Oh, what fun it is to ride
+In a one horse open sleigh! ''')
+
+'''
+notes:
+triple quotes are used for multi-line strings. 
+they let you print text in multiple lines easily.
+great for poems, lyrics, or simple formatting.
+
+'''
+# thanks for reading
